f1775e27ddae8c2de6aa800ac0466b175048fdae
Sooriya <158829004+Sooriya04@users.noreply.github.com>
2024-08-09 19:55:56 +0530

    Add files via upload

---
+++ app/main.py
@@ -0,0 +1,31 @@
+#GO THROUGH README.md FOR YOUR FOR DOWNLOADING MODULES AND GETTING AN API
+from requests import *
+from twilio.rest import Client
+
+def send_sms():
+    account_sid = "" #YOUR TWILIO ACCOUNT SID
+    auth_token = "" #YOUR TWILIO AUTHENTICATION TOKEN
+    client = Client(account_sid, auth_token)
+    try:
+        message = client.messages.create(
+            body="Today will be rainny so Don't forgot to get the Umberalla",
+            from_= "" , #TWILIO NUMBER
+            to="" #YOUR MOBILE NUMBER
+        )
+    except Exception as e:
+        print(f"Error: {e}")
+
+lat = #your lat
+long = #your log
+api_key = #your api key
+params = {
+    'lat': lat,
+    'lon': long,
+    'appid': api_key,
+    }
+api = get(url = "https://api.openweathermap.org/data/2.5/weather?", params = params)
+data_file = api.json()
+data = data_file["weather"][0]["main"]
+if data == "Rain":
+    send_sms()
+    print("SMS is send")
